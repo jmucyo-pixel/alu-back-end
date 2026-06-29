3bc94ba654fd063792ffc8dbe0d61c8628c04e97
jmucyo-pixel <j.mucyo@alustudent.com>
2026-06-29 21:14:21 +0200

    4/4

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Export all employees TODO data to a single JSON file."""
+import json
+import requests
+
+
+if __name__ == "__main__":
+    base = "https://jsonplaceholder.typicode.com"
+
+    users = requests.get("{}/users".format(base)).json()
+    todos = requests.get("{}/todos".format(base)).json()
+
+    data = {}
+    for user in users:
+        uid = user.get("id")
+        username = user.get("username")
+        data[str(uid)] = [
+            {
+                "username": username,
+                "task": t.get("title"),
+                "completed": t.get("completed")
+            }
+            for t in todos if t.get("userId") == uid
+        ]
+
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(data, f)
