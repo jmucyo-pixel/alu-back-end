81787ee57396ab018197f129b6f214e8b37ad7b5
joelm <j.mucyo@alustudent.com>
2026-06-29 21:43:45 +0200

    2/4

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -8,15 +8,12 @@ import sys
 if __name__ == "__main__":
     employee_id = int(sys.argv[1])
     base = "https://jsonplaceholder.typicode.com"
-
     user = requests.get("{}/users/{}".format(base, employee_id)).json()
     todos = requests.get(
         "{}/todos".format(base), params={"userId": employee_id}
     ).json()
-
     username = user.get("username")
     filename = "{}.csv".format(employee_id)
-
     with open(filename, "w", newline="") as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for task in todos:
